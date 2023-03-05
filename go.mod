c9cae384532f14c15b16c704fbf3637a8cb7031a
Marc <71778381+marchoffmann07@users.noreply.github.com>
2023-03-05 16:36:32 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/marchoffmann07/chalk-advanced-go
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,15 @@
+package ChalkAdvancedGo
+
+import "log"
+
+func LogInfo(message string) {
+	log.Printf("INFO | %v", message)
+}
+
+func LogWarning(message string) {
+	log.Printf("WARN | %v", message)
+}
+
+func LogError(message string) {
+	log.Printf("ERROR | %v", message)
+}
